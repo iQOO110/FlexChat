1b88adfd44153cbe28cb03c3918f3bdedbcab915
Ychu <779504983@qq.com>
2026-07-27 11:19:19 +0800

    更新 FlexChatApplication.java

--- backend/src/main/java/com/flexchat/FlexChatApplication.java
+++ backend/src/main/java/com/flexchat/FlexChatApplication.java
@@ -1,1 +1,12 @@
 
+package com.flexchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlexChatApplication.class, args);
+    }
+}
